7ef32157c8b078750bd6f05fee9958c18e3a9208
SeymaDemir <seymademir35@gmail.com>
2022-12-12 23:27:56 +0300

    fetch is okay

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 "use strict";
+const APIUrl = "https://api.adviceslip.com/advice"
 
 const body = document.querySelector("body")
 
+const fetchAdvice = async() => {
+    const result = await fetch(APIUrl).then((res) => res.json()).then((data) => console.log(data.slip.advice))
+    // setTimeout(() => {
+    //     console.log(advice) // state is "fulfilled" or "rejected"
+    //   }, 5000);
+}
+//fetchAdvice()
+
 const enterancePage = () => {
+    fetchAdvice();
     const enterance = document.createElement("h1")
-    enterance.textContent = "hi welcome"
+    enterance.textContent = ""
     body.appendChild(enterance)
 
 }
