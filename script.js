9dfd5c3ccc0571cfc2187bfa7cb9e461c0e25b96
SeymaDemir <seymademir35@gmail.com>
2022-12-13 08:38:08 +0300

    displayed fetching advice

--- script.js
+++ script.js
@@ -2,21 +2,33 @@
 const APIUrl = "https://api.adviceslip.com/advice"
 
 const body = document.querySelector("body")
+let adviceHolder;
+
+const initial = () => {
+    fetchAdvice();
+}
 
 const fetchAdvice = async() => {
-    const result = await fetch(APIUrl).then((res) => res.json()).then((data) => console.log(data.slip.advice))
+    const result = await fetch(APIUrl).then((res) => res.json()).then((data) => passAdviceToHolder(data.slip.advice))
     // setTimeout(() => {
     //     console.log(advice) // state is "fulfilled" or "rejected"
     //   }, 5000);
 }
 //fetchAdvice()
 
-const enterancePage = () => {
-    fetchAdvice();
+function passAdviceToHolder(newAdvice){
+    adviceHolder = newAdvice
+    //return adviceHolder
+    enterancePage(adviceHolder)
+}
+
+const enterancePage = (adviceHolder) => {
     const enterance = document.createElement("h1")
-    enterance.textContent = ""
+    enterance.innerHTML = adviceHolder
     body.appendChild(enterance)
 
 }
 
-document.addEventListener("DOMContentLoaded", enterancePage);
+
+
+document.addEventListener("DOMContentLoaded",initial );
