abb757adde3c0af6a607914eaf712236f40a95b9
SeymaDemir <seymademir35@gmail.com>
2022-12-13 23:57:07 +0300

    added some CSS and advice ID

--- script.js
+++ script.js
@@ -2,30 +2,40 @@
 const APIUrl = "https://api.adviceslip.com/advice"
 
 const cardDiv = document.getElementById("advice-card")
+const idDiv = document.getElementById("advice-id")
+
 let adviceHolder;
+let idHolder;
 
 const initial = () => {
     fetchAdvice();
 }
 
 const fetchAdvice = async() => {
-    const result = await fetch(APIUrl).then((res) => res.json()).then((data) => passAdviceToHolder(data.slip.advice))
+    const result = await fetch(APIUrl).then((res) => res.json()).then((data) => passAdviceToHolder(data.slip.advice, data.slip.id))
     // setTimeout(() => {
     //     console.log(advice) // state is "fulfilled" or "rejected"
     //   }, 5000);
 }
 //fetchAdvice()
 
-function passAdviceToHolder(newAdvice){
+function passAdviceToHolder(newAdvice, id){
     adviceHolder = newAdvice
+    idHolder = id;
     //return adviceHolder
-    enterancePage(adviceHolder)
+    enterancePage(adviceHolder, idHolder)
 }
 
-const enterancePage = (adviceHolder) => {
+const enterancePage = (adviceHolder, idHolder) => {
+    //element for advice id
+    const idNum = document.createElement("p")
+    idNum.id = "advice-id-text"
+    idNum.innerHTML = `ADVICE #${idHolder}`
+    idDiv.appendChild(idNum)
+    //element for advice
     const enterance = document.createElement("p")
     enterance.id = "advice-p-tag"
-    enterance.innerHTML = adviceHolder
+    enterance.innerHTML = `"${adviceHolder}"`
     cardDiv.appendChild(enterance)
 
 }
